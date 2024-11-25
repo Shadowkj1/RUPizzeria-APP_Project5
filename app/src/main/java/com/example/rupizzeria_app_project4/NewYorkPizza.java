ac34721422955302375d0296094af6f2bb903dc0
Shadowkj1 <keshankkj@gmail.com>
2024-11-25 18:56:07 -0500

    chip selection works. Cannot select more than 7 toppings for build your own (well it was implemented for all of them).

--- app/src/main/java/com/example/rupizzeria_app_project4/NewYorkPizza.java
+++ app/src/main/java/com/example/rupizzeria_app_project4/NewYorkPizza.java
@@ -1,12 +1,17 @@
 package com.example.rupizzeria_app_project4;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.chip.Chip;
 
 import java.util.ArrayList;
 
@@ -31,5 +36,33 @@ public class NewYorkPizza extends AppCompatActivity {
         SingletonDataStorage GlobalPizzaData = SingletonDataStorage.getInstance();
         ArrayList<Pizza> currentOrder = GlobalPizzaData.getCurrentOrder();
         ArrayList<Order> orderHistory = GlobalPizzaData.getOrderHistory();
+
+        //pass the c
+        preventTooManyToppings(findViewById(R.id.chipGroup_toppings), 6);
+    }
+
+    public void preventTooManyToppings(View view, int maxToppings) {
+        //code to prevent user from adding too many toppings
+        ChipGroup toppings = (ChipGroup) view;
+        toppings.setOnCheckedStateChangeListener((group, checkedId) -> {
+            if (checkedId.size() > maxToppings) {
+                for (int i=0; i<toppings.getChildCount(); i++) {
+                    Chip chip = (Chip) toppings.getChildAt(i);
+                    if (!chip.isChecked()) {
+                        chip.setEnabled(false);
+                    }
+                }
+                Toast.makeText(this, "You can only select " + (maxToppings+1)
+                        + " toppings", Toast.LENGTH_SHORT).show();
+            } else {
+                for (int i=0; i<toppings.getChildCount(); i++) {
+                    Chip chip = (Chip) toppings.getChildAt(i);
+                    chip.setEnabled(true);
+                }
+            }
+        });
+
+
+
     }
 }
